be89c5fe571c7b2a78b9cbcb09786fdc8d238eb6
NickW09 <nickcreo@yahoo.com>
2022-10-23 11:44:34 -0400

    Debug

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "..\libs\StringBuffer\StringBuffer.h"
 #include "..\libs\ToDoBuffer\ToDoBuffer.h"
 #include "..\libs\SymbolTable\SymbolTable.h"
+#include "..\libs\Debug\Debug.h"
 
 int main() {
     
@@ -18,17 +19,17 @@ int main() {
 
     // ------------------------- Parser Code -------------------------
 
-    InstructionBuffer* instrBuf = InstructionBuffer::getInstance();
-    SymbolTable* symTable = SymbolTable::getInstance();
-    StringBuffer* strBuf = StringBuffer::getInstance();
-    ToDoBuffer* toDoBuf = ToDoBuffer::getInstance();
-    Parser* parser = Parser::getInstance(input, output, instrBuf, symTable, strBuf, toDoBuf);
-    parser->beginParser();
+    //InstructionBuffer* instrBuf = InstructionBuffer::getInstance();
+    //SymbolTable* symTable = SymbolTable::getInstance();
+    //StringBuffer* strBuf = StringBuffer::getInstance();
+    //ToDoBuffer* toDoBuf = ToDoBuffer::getInstance();
+    //Parser* parser = Parser::getInstance(input, output, instrBuf, symTable, strBuf, toDoBuf);
+    //parser->beginParser();
     
     //parse.beginParser(filename);
 
     // ------------------- Virtual Machine Code ----------------------
 
     // -------------------------- Testing ----------------------------
-
+    Debug::test("test_inputs/input.txt", "output.txt", "test_inputs/input.txt");
 }
